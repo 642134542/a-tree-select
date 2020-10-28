98aeb54a6348031c3c780828c9bf2445df5927b6
hqh <642134542@qq.com>
2020-10-28 17:02:48 +0800

    first commit

--- examples/main.js
+++ examples/main.js
@@ -1,7 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
 
+import TreeSelect from "../packages/index";
+
 Vue.config.productionTip = false
+
+Vue.use(TreeSelect);
 
 new Vue({
   render: h => h(App),
